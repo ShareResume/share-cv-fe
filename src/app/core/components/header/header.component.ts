8ea10329b54b6321bb543317e037d3aef1ac8001
Avch Greg <avchargrisha@gmail.com>
2025-03-27 18:26:11 +0100

    feat(core): add header and footer

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ButtonComponent } from '../../../reusable/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [
+    ButtonComponent, 
+    RouterLink, 
+    RouterLinkActive, 
+    CommonModule
+  ],
 })
 export class HeaderComponent {
+  navItems = [
+    { label: 'Overview', route: '/overview' },
+    { label: 'Browse', route: '/browse' },
+    { label: 'Insights', route: '/insights' },
+    { label: 'Forum', route: '/forum' }
+  ];
 
   constructor() { }
-
 }
