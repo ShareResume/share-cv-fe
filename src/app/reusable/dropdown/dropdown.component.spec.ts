7fb96e35172afb21738bc0abfbf535e3f2a0586a
Avch Greg <avchargrisha@gmail.com>
2025-04-06 16:08:23 +0200

    fix: tests

--- src/app/reusable/dropdown/dropdown.component.spec.ts
+++ src/app/reusable/dropdown/dropdown.component.spec.ts
@@ -5,8 +5,24 @@ import { By } from '@angular/platform-browser';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
 import { Status } from '../models/dropdown.model';
+import { Component } from '@angular/core';
 
-describe('DropdownComponent', () => {
+@Component({
+  template: `
+    <app-dropdown 
+      [options]="options" 
+      [multiple]="multiple">
+    </app-dropdown>
+  `,
+  standalone: true,
+  imports: [DropdownComponent]
+})
+class TestHostComponent {
+  options: Status[] = [];
+  multiple = true;
+}
+
+describe('DropdownComponent Basic', () => {
   let component: DropdownComponent;
   let fixture: ComponentFixture<DropdownComponent>;
   const testOptions: Status[] = [{
@@ -25,81 +41,117 @@ describe('DropdownComponent', () => {
 
     fixture = TestBed.createComponent(DropdownComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 
   it('should show options', () => {
     fixture.componentRef.setInput('options', testOptions);
-    component.ngOnInit();
     fixture.detectChanges();
+    component.ngOnInit();
 
     fixture.debugElement.query(By.css('.dropdown')).nativeElement?.click();
     fixture.detectChanges();
 
     expect(fixture.debugElement.query(By.css('.custom')).nativeElement.innerText).toContain('Name')
   });
+});
 
-  it('should select option in multiple mode', () => {
-    fixture.componentRef.setInput('options', testOptions);
-    fixture.componentRef.setInput('multiple', true);
+describe('DropdownComponent with multiple selection', () => {
+  let hostComponent: TestHostComponent;
+  let hostFixture: ComponentFixture<TestHostComponent>;
+  let dropdownComponent: DropdownComponent;
+  const testOptions: Status[] = [{
+    value: 'ID',
+    viewValue: 'Name',
+  }];
 
-    component.ngOnInit();
-    fixture.detectChanges();
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      imports: [TestHostComponent, DropdownComponent, MatIconTestingModule],
+      providers: [
+        provideNoopAnimations(),
+      ],
+    })
+    .compileComponents();
 
-    fixture.debugElement.query(By.css('.dropdown')).nativeElement?.click();
-    fixture.detectChanges();
-
-    fixture.debugElement.query(By.css('mat-option')).nativeElement?.click();
-    fixture.detectChanges();
-
-    expect(component.form?.get('status')?.value).toEqual(['ID']);
+    hostFixture = TestBed.createComponent(TestHostComponent);
+    hostComponent = hostFixture.componentInstance;
+    hostComponent.options = testOptions;
+    hostComponent.multiple = true;
+    hostFixture.detectChanges();
+    
+    dropdownComponent = hostFixture.debugElement.query(
+      By.directive(DropdownComponent)
+    ).componentInstance;
   });
 
-  it('should select option in single mode', () => {
-    fixture.componentRef.setInput('options', testOptions);
-    fixture.componentRef.setInput('multiple', false);
+  it('should select option in multiple mode', () => {
+    hostFixture.debugElement.query(By.css('.dropdown')).nativeElement?.click();
+    hostFixture.detectChanges();
 
-    component.ngOnInit();
-    fixture.detectChanges();
+    hostFixture.debugElement.query(By.css('mat-option')).nativeElement?.click();
+    hostFixture.detectChanges();
 
-    fixture.debugElement.query(By.css('.dropdown')).nativeElement?.click();
-    fixture.detectChanges();
-
-    fixture.debugElement.query(By.css('mat-option')).nativeElement?.click();
-    fixture.detectChanges();
-
-    expect(component.form?.get('status')?.value).toEqual('ID');
+    expect(dropdownComponent.form?.get('status')?.value).toEqual(['ID']);
   });
 
   it('should show checkbox for multiple selection', () => {
-    fixture.componentRef.setInput('options', testOptions);
-    fixture.componentRef.setInput('multiple', true);
+    hostFixture.debugElement.query(By.css('.dropdown')).nativeElement?.click();
+    hostFixture.detectChanges();
 
-    component.ngOnInit();
-    fixture.detectChanges();
+    expect(hostFixture.debugElement.query(By.css('.custom mat-pseudo-checkbox'))).toBeTruthy();
+  });
+});
 
-    fixture.debugElement.query(By.css('.dropdown')).nativeElement?.click();
-    fixture.detectChanges();
+describe('DropdownComponent with single selection', () => {
+  let hostComponent: TestHostComponent;
+  let hostFixture: ComponentFixture<TestHostComponent>;
+  let dropdownComponent: DropdownComponent;
+  const testOptions: Status[] = [{
+    value: 'ID',
+    viewValue: 'Name',
+  }];
 
-    expect(fixture.debugElement.query(By.css('.custom mat-pseudo-checkbox'))).toBeTruthy();
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      imports: [TestHostComponent, DropdownComponent, MatIconTestingModule],
+      providers: [
+        provideNoopAnimations(),
+      ],
+    })
+    .compileComponents();
+
+    hostFixture = TestBed.createComponent(TestHostComponent);
+    hostComponent = hostFixture.componentInstance;
+    hostComponent.options = testOptions;
+    hostComponent.multiple = false;
+    hostFixture.detectChanges();
+    
+    dropdownComponent = hostFixture.debugElement.query(
+      By.directive(DropdownComponent)
+    ).componentInstance;
+  });
+
+  it('should select option in single mode', () => {
+    hostFixture.debugElement.query(By.css('.dropdown')).nativeElement?.click();
+    hostFixture.detectChanges();
+
+    hostFixture.debugElement.query(By.css('mat-option')).nativeElement?.click();
+    hostFixture.detectChanges();
+
+    expect(dropdownComponent.form?.get('status')?.value).toEqual('ID');
   });
 
   it('should not show checkbox for single selection', () => {
-    fixture.componentRef.setInput('options', testOptions);
-    fixture.componentRef.setInput('multiple', false);
-
-    component.ngOnInit();
-    fixture.detectChanges();
-
-    fixture.debugElement.query(By.css('.dropdown')).nativeElement?.click();
-    fixture.detectChanges();
+    hostFixture.debugElement.query(By.css('.dropdown')).nativeElement?.click();
+    hostFixture.detectChanges();
 
     // In single selection mode, the mat-pseudo-checkbox should not be displayed
-    const pseudoCheckboxes = fixture.debugElement.queryAll(By.css('.mat-mdc-option:not(.mat-mdc-option-multiple) .mat-pseudo-checkbox'));
+    const pseudoCheckboxes = hostFixture.debugElement.queryAll(By.css('.mat-mdc-option:not(.mat-mdc-option-multiple) .mat-pseudo-checkbox'));
     expect(pseudoCheckboxes.length).toBe(0);
   });
 });
