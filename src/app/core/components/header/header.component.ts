bce0fc942fd0a5fa3a2f83dbba998bc6d3db5fd0
Avch Greg <avchargrisha@gmail.com>
2025-05-01 18:30:43 +0200

    feat(header): enhance header component with admin link and styling

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -3,38 +3,42 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ButtonComponent } from '../../../reusable/button/button.component';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import { UserRoleEnum } from '../../enums/user-role.enum';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [
-    ButtonComponent, 
-    RouterLink, 
-    RouterLinkActive, 
-    CommonModule,
-  ],
+  imports: [ButtonComponent, RouterLink, RouterLinkActive, CommonModule],
 })
 export class HeaderComponent {
   private authService = inject(AuthService);
-  
+
   navItems = [
     {
- label: 'Overview', route: '/overview', 
-},
+      label: 'Overview',
+      route: '/overview',
+    },
     {
- label: 'Browse', route: '/browse', 
-},
+      label: 'Browse',
+      route: '/browse',
+    },
     {
- label: 'Insights', route: '/insights', 
-},
+      label: 'Insights',
+      route: '/insights',
+    },
     {
- label: 'Forum', route: '/forum', 
-},
+      label: 'Forum',
+      route: '/forum',
+    },
   ];
 
   get isAuthenticated(): boolean {
     return this.authService.isAuthenticated;
+  }
+
+  get isAdmin(): boolean {
+    return this.authService.userRole === UserRoleEnum.ADMIN;
   }
 }
